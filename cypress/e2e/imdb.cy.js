7ae9ee7c57619a770506dc93c7bdda1fe5dccd47
Leandro Maciel <lmaciel.tech@gmail.com>
2025-02-14 11:50:29 -0300

    adicionar teste para obter listagem de resultados na busca

--- cypress/e2e/imdb.cy.js
+++ cypress/e2e/imdb.cy.js
@@ -1,9 +1,10 @@
+
 describe('OMDB - Search by Id or Title', () => {
 
     context('Busca de títulos por ID ou Titiulo', () => {
-        var param = {
-            titulo:'12 Angry Men',
-            id: 'tt0050083', 
+        const param = {
+            titulo: '12 Angry Men',
+            id: 'tt0050083',
             type: 'movie',
             year: 1,
             plot: 'sort',
@@ -46,11 +47,11 @@ describe('OMDB - Search by Id or Title', () => {
             })
         })
 
-        it('Deve retornar filme na pesquisa por título', ()=>{
+        it('Deve retornar filme na pesquisa por título', () => {
             cy.request({
                 method: 'GET',
                 url: host + `?t=${param.titulo}&type=${param.type}&r=${param.result}&apikey=${param.key}`
-            }).then((response)=>{
+            }).then((response) => {
                 expect(response.body.Title).to.be.eq('12 Angry Men')
                 expect(response.body.Year).to.be.eq('1957')
                 expect(response.body.Rated).to.be.eq('Approved')
@@ -78,6 +79,64 @@ describe('OMDB - Search by Id or Title', () => {
                 expect(response.body.Response).to.eq('True')
             })
         })
+
+        it('Deve retornar filme na pesquisa por parte do título', () => {
+            let titulo = 'Schindler'
+            cy.request({
+                method: 'GET',
+                url: host + `?t=${titulo}&type=${param.type}&r=${param.result}&apikey=${param.key}`
+            }).then((response) => {
+                expect(response.body.Title).contains(titulo)
+                expect(response.body.Year).not.empty
+                expect(response.body.Rated).not.empty
+                expect(response.body.Released).not.empty
+                expect(response.body.Runtime).not.empty
+                expect(response.body.Genre).not.empty
+                expect(response.body.Director).not.empty
+                expect(response.body.Writer).not.empty
+                expect(response.body.Actors).not.empty
+                expect(response.body.Plot).not.empty
+                expect(response.body.Language).not.empty
+                expect(response.body.Country).not.empty
+                expect(response.body.Awards).not.empty
+                expect(response.body.Poster).not.empty
+                expect(response.body.Ratings).not.empty
+                expect(response.body.Metascore).not.empty
+                expect(response.body.imdbRating).not.empty
+                expect(response.body.imdbVotes).not.empty
+                expect(response.body.imdbID).not.empty
+                expect(response.body.Type).not.empty
+                expect(response.body.DVD).not.empty
+                expect(response.body.BoxOffice).not.empty
+                expect(response.body.Production).not.empty
+                expect(response.body.Website).not.empty
+                expect(response.body.Response).to.eq('True')
+            })
+        })
+
+        it('Deve retornar listagem de resultados para a busca por título do filme', () => {
+            let titulo = 'the fast and the furious'
+            cy.request({
+                method: 'GET',
+                url: host + `?s=${titulo}&type=${param.type}&r=${param.result}&apikey=${param.key}`
+            }).then((response) =>{
+                let filmes = response.body.Search
+                expect(response.body.Search).not.empty
+                expect(response.body.totalResults).not.empty
+                expect(response.body.Response).to.eq('True')
+                
+                filmes.forEach(filme => {
+                    expect(filme.Title).not.empty
+                    expect(filme.Year).not.empty
+                    expect(filme.imdbID).not.empty
+                    expect(filme.Type).not.empty
+                    expect(filme.Poster).not.empty
+                });
+
+            })
+        })
+
+
 
     })
 
